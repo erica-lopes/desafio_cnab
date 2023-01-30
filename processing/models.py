f5cb57515af9a9d206398aaa22df3f365f7365c5
Erica Lopes <ericalopes@outlook.com>
2023-01-29 23:13:32 -0300

    feat: adicionando views

--- processing/models.py
+++ processing/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 
 class DataPost(models.Model):
-    content = models.FileField()
+    type = models.CharField(max_length=1)
+    date = models.CharField(max_length=8)
+    value = models.CharField(max_length=10)
+    cpf = models.CharField(max_length=11)
+    card = models.CharField(max_length=12)
+    hour = models.CharField(max_length=6)
+    owner = models.CharField(max_length=14)
+    store_name = models.CharField(max_length=19)
+
+    def __str__(self) -> str:
+        return self.store_name
--- processing/views.py
+++ processing/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render, redirect
-from .forms import UploadFile
-from .utils import handle_uploaded_file
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.db.models import Sum
+from .forms import UploadFileForm
+from .models import DataPost
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def upload_file(request):
     if request.method == "POST":
-        form = UploadFile(request.POST, request.FILES)
+        form = UploadFileForm(request.POST, request.FILES)
 
         if form.is_valid():
-            handle_uploaded_file(request.FILES["file"])
-            return redirect("upload/")
+            uploaded_file = request.FILES["file"]
+
+            for line in uploaded_file:
+                str_line = line.decode()
+
+                type = str_line[0]
+                date = f"{str_line[1:5]}-{str_line[5:7]}-{str_line[7:9]}"
+                value = str_line[9:19]
+                cpf = str_line[19:30]
+                card = str_line[30:42]
+                hour = f"{str_line[42:44]}:{str_line[44:46]}:{str_line[46:48]}",
+                owner = str_line[48:62]
+                store_name = str_line[62:80]
+
+                DataPost.objects.create(
+                    type=type,
+                    date=date,
+                    value=int(value) / 100,
+                    cpf=cpf,
+                    card=card,
+                    hour=hour,
+                    owner=owner,
+                    store_name=store_name,
+                )
+                return HttpResponseRedirect("list/")
+
     else:
-        form = UploadFile()
+        form = UploadFileForm()
     return render(request, "cnab/upload.html", {"form": form})
+
+
+def render(request):
+    stores = DataPost.objects.all()
+    stores_list = []
+
+    for name in stores:
+        store = DataPost.objects.filter(store_name=name)[0]
+        total_balance = DataPost.objects.filter(store_name=name).aggregate(total=Sum("value"))
+        store.total = total_balance["total"] / 100
+        stores_list.append(store)
+
+    return render(request, "cnab/list.html", {"stores_list": stores_list})
